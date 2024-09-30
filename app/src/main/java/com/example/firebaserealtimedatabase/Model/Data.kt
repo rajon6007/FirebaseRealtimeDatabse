79f0ef41d31f39aaa1da628c76c5bf633ef6c2bb
rajon6007 <paulrajon412@gmail.com>
2024-09-30 13:24:51 +0600

    Firebase Realtime database

---
+++ app/src/main/java/com/example/firebaserealtimedatabase/Model/Data.kt
@@ -0,0 +1,10 @@
+package com.example.firebaserealtimedatabase.Model
+
+data class Data(
+    var id: String? = null,
+    val stuId: String? = null,
+    val name: String? = null,
+    val email: String? = null,
+    val subject: String? = null,
+    val birthdate: String? = null
+)
